c958df1e18c9f57834bb011c40389e2709259a65
Dawid Kaszuba <d.kaszuba89@gmail.com>
2026-02-16 22:19:46 +0100

    first commit

---
+++ src/main/java/pl/dawidkaszuba/acts_ingester/ActsIngesterApplication.java
@@ -0,0 +1,13 @@
+package pl.dawidkaszuba.acts_ingester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActsIngesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActsIngesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dawidkaszuba/acts_ingester/ActsIngesterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dawidkaszuba.acts_ingester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActsIngesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
